9353b157ad6a9531f728f015b77c25bf67e3556c
dinhvandong <dongvan.ict3@gmail.com>
2025-02-18 11:15:25 +0700

    updatre code banner + header

--- LayoutDemo/app/src/main/java/com/btec/fpt/layoutdemo/LoginActivity.java
+++ LayoutDemo/app/src/main/java/com/btec/fpt/layoutdemo/LoginActivity.java
@@ -3,6 +3,7 @@ package com.btec.fpt.layoutdemo;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -15,6 +16,7 @@ public class LoginActivity extends AppCompatActivity {
 
 
     TextView tvRegister;
+    Button btnLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +28,17 @@ public class LoginActivity extends AppCompatActivity {
             return insets;
         });
 
+        btnLogin = (Button)findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+
+                startActivity(intent);
+
+            }
+        });
         tvRegister = (TextView) findViewById(R.id.tv_register);
 
         tvRegister.setOnClickListener(new View.OnClickListener() {
